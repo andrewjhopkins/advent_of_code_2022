4f21765c01839d5f126ae094d4c68f35d89e81a7
Andrew Hopkins <ahopkins0929@gmail.com>
2022-12-06 14:49:23 +0000

    day 6

---
+++ 6_day/part_2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        cout << "Usage: .exe <file input>\n";
+        return 0;
+    }
+
+    ifstream infile(argv[1]);
+
+    string line;
+    getline(infile, line);
+
+    int window_start = 0;
+
+    map<char, int> charIndex;
+
+    for (int window_end = 0; window_end < line.size(); window_end++) {
+      char currentChar = line.at(window_end);
+
+      if (charIndex.find(currentChar) != charIndex.end() && charIndex[currentChar] >= window_start) {
+        window_start = charIndex[currentChar] + 1;
+      }
+
+      charIndex[currentChar] = window_end;
+
+      if ((window_end - window_start + 1) == 14) {
+        cout << window_end + 1 << "\n";
+        return 0;
+      }
+    }
+
+    return 0;
+}
